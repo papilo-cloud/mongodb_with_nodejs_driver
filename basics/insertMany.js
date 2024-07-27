e30807a2f965390a16bdeec02b4534e8bf34df9d
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-07-27 23:03:47 +0100

    insertMany

--- basics/insertMany.js
+++ basics/insertMany.js
@@ -15,52 +15,52 @@ async function insertMany() {
 
         const datas = [
             {
-                _id: 1,
-                account_id: 'MBD578458784',
+                _id: 7,
+                account_id: 'MBD578474883',
                 account_type: 'savings',
-                account_holder: 'Naja Petersen',
-                balance: 2891.4,
-                transfers_complete: ['TR12345', 'TR28376', 'TR29901']
+                account_holder: 'Pete Oliver',
+                balance: 1491.4,
+                transfers_complete: ['TR12345', 'TR29901']
             },
             {
-                _id: 2,
-                account_id: 'BGT900458866',
+                _id: 8,
+                account_id: 'BhT0993398376',
                 account_type: 'current',
-                account_holder: 'Sam Joe',
-                balance: 3782.4,
-                transfers_complete: ['TR12345', 'TR28376', 'TR29901']
+                account_holder: 'Michael Mane',
+                balance: 382.4,
+                transfers_complete: []
             },
             {
-                _id: 3,
+                _id: 9,
                 account_id: 'DWD548858709',
                 account_type: 'savings',
-                account_holder: 'Dan Wiskey',
+                account_holder: 'Drag Wiz',
                 balance: 1222.9,
-                transfers_complete: ['TR12345', 'TR28376', 'TR29901']
+                transfers_complete: ['TR29901']
             },
             {
-                _id: 4,
+                _id: 10,
                 account_id: 'WED389758094',
-                account_type: 'savings',
-                account_holder: 'Hajara Habibu',
-                balance: 9220.4,
-                transfers_complete: ['TR12345', 'TR28376', 'TR29901']
-            },
-            {
-                _id: 5,
-                account_id: 'REF488940784',
-                account_type: 'savings',
-                account_holder: 'Sara Isak',
-                balance: 9893.1,
-                transfers_complete: ['TR12345', 'TR28376', 'TR29901']
-            },
-            {
-                _id: 6,
-                account_id: 'CFD578412673',
                 account_type: 'current',
-                account_holder: 'Abu John',
-                balance: 12021.0,
-                transfers_complete: ['TR12345', 'TR28376', 'TR29901']
+                account_holder: 'Hajara Habibu',
+                balance: 920.4,
+                transfers_complete: []
+            },
+            {
+                _id: 11,
+                account_id: 'RGT88785644332',
+                account_type: 'savings',
+                account_holder: 'Mike Gosing',
+                balance: 1102.1,
+                transfers_complete: []
+            },
+            {
+                _id: 12,
+                account_id: 'MNh3437738498',
+                account_type: 'current',
+                account_holder: 'Ola Janet',
+                balance: 891.0,
+                transfers_complete: ['TR12345']
             },
         ]
 
